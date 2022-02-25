3891493c175599519ba81fd4f7e82bfa5f01c624
Anartz Mugika Ledo <mugan86@gmail.com>
2022-02-25 20:05:01 +0100

    refactor: Implementar sin datastore y con pipe y tap

--- src/app/@pages/movies/movies.service.ts
+++ src/app/@pages/movies/movies.service.ts
@@ -1,40 +1,56 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  EMPTY,
+  Subscription,
+  tap,
+  throwError,
+} from 'rxjs';
 import { IMovie } from './movies.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesService {
+  private baseUrl = 'http://localhost:3000';
+
   private loadingData$ = new BehaviorSubject<boolean>(true);
   private movies$ = new BehaviorSubject<IMovie[]>([]);
-  private baseUrl = 'http://localhost:3000';
-  private dataStore: { movies: IMovie[] } = { movies: [] };
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {}
 
   get loadingData() {
     return this.loadingData$.asObservable();
   }
+
   get movies() {
     return this.movies$.asObservable();
   }
+
   loadAll() {
-    this.loadingData$.next(true);
-    this.http.get<IMovie[]>(`${this.baseUrl}/movies`)
-    /*.pipe(
-      catchError(err => of([]))
-    )*/
-    .subscribe({
-      next: (data: any) => {
-        this.dataStore.movies = data;
-        this.movies$.next(Object.assign({}, this.dataStore).movies);
-      },
-      complete: () => {
-        console.log('Complete');
-        this.loadingData$.next(false);
-      },
-      error: (e: Error) => console.log(e),
-    });
+    const url = `${this.baseUrl}/movies`;
+
+    const sub$: Subscription = this.http
+      .get<IMovie[]>(url)
+      .pipe(
+        tap(() => this.loadingData$.next(true)),
+        tap((movies) => this.movies$.next(movies)),
+        tap(() => this.loadingData$.next(false)),
+        catchError(this.handleError)
+      )
+      .subscribe({
+        complete: () => sub$.unsubscribe(),
+        error: () => sub$.unsubscribe(),
+      });
+  }
+
+  /**
+   * Sistema muy basico de manejo de errores
+   */
+  handleError(error: Error) {
+    console.log(error);
+    return throwError(() => EMPTY);
   }
 }
