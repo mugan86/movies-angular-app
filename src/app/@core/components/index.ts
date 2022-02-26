37fd1241d44922ef760896a252260b83dd2aef6e
Anartz Mugika Ledo <mugan86@gmail.com>
2022-02-26 19:12:34 +0100

    refactor: Aplicada refactorización para elementos de carga de tipo de lista

--- src/app/@core/components/index.ts
+++ src/app/@core/components/index.ts
@@ -1,7 +1,7 @@
 import {NavbarModule} from './navbar/navbar.module';
 import {SidebarModule} from './sidebar/sidebar.module';
-import {ContentItemsLoaderModule} from './content-items-loader/content-items-loader.module';
-import {ContentItemLoaderModule} from './content-item-loader/content-item-loader.module';
+import {ContentItemsLoaderModule} from './list-loaders/content-items-loader/content-items-loader.module';
+import {ContentItemLoaderModule} from './list-loaders/content-item-loader/content-item-loader.module';
 
 
 export {
--- src/app/@core/components/list-loaders/content-items-loader/content-items-loader.module.ts
+++ src/app/@core/components/list-loaders/content-items-loader/content-items-loader.module.ts
@@ -1,8 +1,7 @@
-import { ContentItemLoaderModule } from '@core/components/content-item-loader/content-item-loader.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentItemsLoaderComponent } from './content-items-loader.component';
-
+import { ContentItemLoaderModule } from '@core/components/list-loaders/content-item-loader/content-item-loader.module';
 @NgModule({
   declarations: [ContentItemsLoaderComponent],
   imports: [CommonModule, ContentItemLoaderModule],
