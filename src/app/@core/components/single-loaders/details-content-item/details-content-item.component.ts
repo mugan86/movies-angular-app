d165f0fc5b22ca993698e251e1228cefc1cd09a9
Anartz Mugika Ledo <mugan86@gmail.com>
2022-02-28 18:29:33 +0100

    refactor: Aplicado Lint y limpieza de errores y advertencias

--- src/app/@core/components/single-loaders/details-content-item/details-content-item.component.ts
+++ src/app/@core/components/single-loaders/details-content-item/details-content-item.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-details-content-item',
   templateUrl: './details-content-item.component.html',
   styleUrls: ['./details-content-item.component.css']
 })
-export class DetailsContentItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class DetailsContentItemComponent {}
