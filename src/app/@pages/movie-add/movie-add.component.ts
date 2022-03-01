52a795f62859fd94b7eefe4de141a981384bf78d
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-01 20:24:20 +0100

    feature: Refactorización aislando la configuración del formulario

--- src/app/@pages/movie-add/movie-add.component.ts
+++ src/app/@pages/movie-add/movie-add.component.ts
@@ -1,13 +1,13 @@
 import { ICompany } from '@pages/companies/company.interface';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { NavigationService, TitleService } from '@core/services';
 import { TranslateService } from '@ngx-translate/core';
 import { CompaniesService } from '@pages/companies/companies.service';
-import { URL_VALIDATION_REGEX } from '@core/constants/regex';
 import { ActorService } from '@pages/actors/actor.service';
 import { MoviesService } from '@pages/movies/movies.service';
 import { IListField } from '@core/interfaces/form.interface';
+import { configcreateMovieForm } from './form-configs';
 
 @Component({
   selector: 'app-movie-add',
@@ -17,16 +17,16 @@ import { IListField } from '@core/interfaces/form.interface';
 export class MovieAddComponent implements OnInit {
   loading: boolean = true;
   createForm: FormGroup;
-  yearLimit: number = new Date().getFullYear();
   submitted: boolean = false;
+  currentYear = new Date().getFullYear();
   companySelect: ICompany = {
     name: '',
     country: '',
     createYear: -1,
     employees: -1,
     rating: 0,
-    movies: []
-  }
+    movies: [],
+  };
   companiesList: Array<IListField> = [];
   actorsList: Array<IListField> = [];
   genresList: Array<string> = [];
@@ -39,26 +39,7 @@ export class MovieAddComponent implements OnInit {
     private actorService: ActorService,
     private moviesService: MoviesService
   ) {
-    this.createForm = this.formBuilder.group({
-      title: ['', Validators.required],
-      poster: ['', [Validators.required, Validators.pattern(URL_VALIDATION_REGEX)]],
-      genres: [''],
-      company: [null, [Validators.required]],
-      actors: [null, [Validators.required]],
-      year: [
-        `${this.yearLimit}`,
-        [
-          Validators.required,
-          Validators.max(this.yearLimit),
-          Validators.min(2000),
-        ],
-      ],
-      duration: [
-        '120',
-        [Validators.required, Validators.max(300), Validators.min(70)],
-      ],
-      rating: ['5', Validators.required],
-    });
+    this.createForm = this.formBuilder.group(configcreateMovieForm());
     // https://www.positronx.io/angular-url-validation-with-reactive-forms-tutorial/
     // https://www.positronx.io/angular-select-dropdown-with-reactive-forms-examples/
     // https://stackblitz.com/edit/ng-reactive-tpl-forms?file=src%2Fapp%2Freactive-forms%2Freactive-forms.component.html
@@ -70,17 +51,16 @@ export class MovieAddComponent implements OnInit {
 
   ngOnInit() {
     this.loading = true;
-    this.companiesService.names().subscribe(( result: Array<IListField> ) => {
+    this.companiesService.names().subscribe((result: Array<IListField>) => {
       this.companiesList = result;
       this.moviesService.genresList().subscribe((result: Array<string>) => {
         this.genresList = result;
         this.actorService.names().subscribe((result: Array<IListField>) => {
           this.actorsList = result;
           this.loading = false;
-        })
-      })
+        });
+      });
     });
-
   }
 
   get form() {
@@ -105,7 +85,7 @@ export class MovieAddComponent implements OnInit {
     this.createForm.reset();
   }
 
-  addValue(event: any) {
+  addGenre(event: any) {
     console.log(event.target?.value);
     console.log(this.createForm.value);
     // Aqui voy a guardar el valor en lo seleccionado
