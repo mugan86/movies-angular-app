1d6673943d0d3667eb74930b028612d777f763a0
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-01 10:40:09 +0100

    feature: Añadido loader skeleton para formularios

---
+++ src/app/@core/components/form-loaders/simple-loader/simple-loader.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-loader',
+  templateUrl: './simple-loader.component.html',
+  styleUrls: ['./simple-loader.component.css']
+})
+export class SimpleFormLoaderComponent {}
--- src/app/@core/components/index.ts
+++ src/app/@core/components/index.ts
@@ -3,11 +3,13 @@ import {SidebarModule} from './sidebar/sidebar.module';
 import {ContentItemsLoaderModule} from './list-loaders/content-items-loader/content-items-loader.module';
 import {ContentItemLoaderModule} from './list-loaders/content-item-loader/content-item-loader.module';
 import {DetailsContentItemModule} from './single-loaders/details-content-item/details-content-item.module';
+import { SimpleFormLoaderModule } from './form-loaders/simple-loader/simple-loader.module';
 
 export {
     NavbarModule,
     SidebarModule,
     ContentItemLoaderModule,
     ContentItemsLoaderModule,
-    DetailsContentItemModule
+    DetailsContentItemModule,
+    SimpleFormLoaderModule
 }
