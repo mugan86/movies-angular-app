33337f772dcda63c069281423d6189bcf3d3f0c5
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-03 13:39:48 +0100

    refactor: Asignar película a compañía

--- src/app/@pages/companies/companies.service.ts
+++ src/app/@pages/companies/companies.service.ts
@@ -3,8 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BASE_URL } from '@core/constants/api';
 import { Observable } from 'rxjs/internal/Observable';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { IListField } from '@core/interfaces/form.interface';
+import { IMovie } from '@pages/movies/movie.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -25,6 +26,17 @@ export class CompaniesService {
       company
     );
   };
+
+  assignMovieInCompany = (movie: IMovie, companyId: number) => {
+    return this.item(companyId).pipe(
+      switchMap((company) => {
+        return this.edit(companyId, {
+          ...company,
+          movies: [...company?.movies, movie.id],
+        });
+      })
+    );
+  }
 
   names = (): Observable<IListField[]> =>
     this.list().pipe(
