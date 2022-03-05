3d3b61e6a19d0f98c8e6ccd0f047236d964a5252
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-05 12:04:24 +0100

    update: Eliminado método que no se usa actualmente

--- src/app/@core/helpers/i18n.ts
+++ src/app/@core/helpers/i18n.ts
@@ -1,10 +1,5 @@
 import { TranslateLoader } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/@data/i18n/', '.json');
-}
 
 export class CustomTranslateLoader implements TranslateLoader {
   constructor(private http: HttpClient, private prefix: string = 'en') {}
