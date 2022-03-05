94e87b72cd12bd4a3ad1b6e1a8024a78be09cd29
Anartz Mugika Ledo <mugan86@gmail.com>
2022-03-05 12:02:09 +0100

    test: Componente 'app' testeado completamente

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,53 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { createTranslateLoader } from './app.module';
+import { HttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
+
+  let translate: TranslateService;
+  let http: HttpTestingController;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: createTranslateLoader,
+            deps: [HttpClient]
+          }
+        })
       ],
       declarations: [
         AppComponent
       ],
+      providers: [TranslateService]
     }).compileComponents();
+    translate = TestBed.inject(TranslateService);
+    http = TestBed.inject(HttpTestingController);
   });
 
-  it('should create the app', () => {
+  it('Crear la app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as selectedLanguage is 'es'`, () => {
+  it(`Idioma por defecto asignado es 'es'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.selectedLanguage).toEqual('es');
   });
 
-  it('should render title', () => {
+  /*it('should render title ', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('movies_app app is running!');
-  });
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Países del mundo');
+  });*/
 });
